f63c5c44903703de394a82f3b15ff6613655f6cc
Temirlan <temirlan.batchaev@gmail.com>
2021-12-09 19:24:09 +0300

    ready

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+    add_action('wp_enqueue_scripts', 'ojjoscripts');
+    function ojjoscripts () {
+        wp_enqueue_style('ojjo_style', get_template_directory_uri() . '/assets/css/style.min.css');
+        wp_enqueue_script( 'ojjo_script', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), null, true);
+    }
+?>
