31614cc37973d8bef901ae3c67fe744036fefb38
Temirlan <temirlan.batchaev@gmail.com>
2021-12-10 15:20:03 +0300

    made promo

--- header.php
+++ header.php
@@ -8,3 +8,28 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <section class="promo">
+        <header class="header">
+            <div class="container">
+                <nav>
+                    <ul class="header__menu menu">
+                        <li class="menu__item"><a href="#" class="menu__link">Контрагентам</a></li>
+                        <li class="menu__item"><a href="#" class="menu__link">Дизайнерам</a></li>
+                        <li class="menu__item"><a href="#" class="menu__link">Вакансии</a></li>
+                    </ul>
+                </nav>
+                <img src="<?php echo bloginfo('template_url') . '/assets/img/icons/logo.png' ?>" alt="logo" class="header__logo">
+                <div class="header__wrapper">
+                    <div class="header__item">
+                        <a href="#"><img src="<?php echo bloginfo("template_url") . "/assets/img/icons/header/search.png" ?>" alt="search" class="header__img"></a>
+                        <a href="#" class="header__title">Поиск</a>
+                    </div>
+                    <a href="#" class="header__item"><div class="header__title">Вход/Регистрация</div></a>
+                    <div class="header__item">
+                    <a href="#"><img src="<?php echo bloginfo("template_url") . "/assets/img/icons/header/user.png" ?>" alt="user" class="header__img"></a>
+                    <a href="#"><img src="<?php echo bloginfo("template_url") . "/assets/img/icons/header/like.png" ?>" alt="like" class="header__img"></a>
+                    </div>
+                </div>
+            </div>
+        </header>
+ 
--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
     get_header();
-?>
+?>      <div class="container">
+            <h1 class="promo__header">Долго, дорого, богато</h1>
+            <button class="button promo__btn">Каталог изделий</button>
+        </div>
+        <div class="promo__divider">
+        </div>
+        <div class="container">
+            <div class="promo__wrapper">
+                <div class="promo__item"><img src="<?php echo bloginfo("template_url") . '/assets/img/icons/jewellery_btn.png' ?>" alt="jewellery" class="promo__img"></div>
+                <div class="promo__item"><img src="<?php echo bloginfo("template_url") . '/assets/img/icons/jewellery_btn.png' ?>" alt="jewellery" class="promo__img"></div>
+                <div class="promo__item"><img src="<?php echo bloginfo("template_url") . '/assets/img/icons/jewellery_btn.png' ?>" alt="jewellery" class="promo__img"></div>
+                <div class="promo__item"><img src="<?php echo bloginfo("template_url") . '/assets/img/icons/jewellery_btn.png' ?>" alt="jewellery" class="promo__img"></div>
+                <div class="promo__item"><img src="<?php echo bloginfo("template_url") . '/assets/img/icons/jewellery_btn.png' ?>" alt="jewellery" class="promo__img"></div>
+                <div class="promo__item"><img src="<?php echo bloginfo("template_url") . '/assets/img/icons/jewellery_btn.png' ?>" alt="jewellery" class="promo__img"></div>
+            </div>
+        </div>
+    </section>
 <?php
     get_footer();
 ?>
