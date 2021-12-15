1f250bd26bfe399c869994c20cdf5ff9d3e8361e
Temirlan <temirlan.batchaev@gmail.com>
2021-12-15 15:42:16 +0300

    made mailing

--- footer.php
+++ footer.php
@@ -1,3 +1,31 @@
+<div class="mailing">
+    <div class="container">
+        <div class="title title_fz21 title_wht">Полезные советы и персональные предложения</div>
+        <h2 class="title title_fz30 title_wht">Эксклюзивная рассылка</h2>
+        <div class="mailing__wrapper">
+            <div class="mailing__left"><div class="mailing__item">
+                <img src="<?php echo bloginfo('template_url') . '/assets/img/icons/subscribe__ikon.svg' ?>" alt="subscribe__ikon" class="mailing__icon">
+                <div class="mailing__title">Личный менеджер</div>
+            </div>
+            <div class="mailing__item">
+                <img src="<?php echo bloginfo('template_url') . '/assets/img/icons/subscribe__ikon.svg' ?>" alt="subscribe__ikon" class="mailing__icon">
+                <div class="mailing__title">Доставка и оформление</div>
+            </div>
+            <div class="mailing__item">
+                <img src="<?php echo bloginfo('template_url') . '/assets/img/icons/subscribe__ikon.svg' ?>" alt="subscribe__ikon" class="mailing__icon">
+                <div class="mailing__title">Индивидуальный дизаин</div>
+            </div></div>
+            <div class="mailing__right">
+                <form action="#" class="mailing-form">
+                    <div class="mailing-form__item">
+                        <input type="email" placeholder="ВАШ E-MAIL" class="mailing-form__input">
+                        <button class="button mailing-form__btn">Отправить</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <?php wp_footer();?>
 </body>
 </html>
