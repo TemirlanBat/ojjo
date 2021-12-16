dbd1f5490fd46ef5b1767c9cf6153b484a0e77d6
Temirlan <temirlan.batchaev@gmail.com>
2021-12-16 17:46:12 +0300

    made footer

--- footer.php
+++ footer.php
@@ -26,6 +26,66 @@
         </div>
     </div>
 </div>
+<footer class="footer">
+    <div class="container">
+        <div class="footer__wrapper">
+            <div class="footer__item">
+                <div class="footer__title">Полезные ссылки</div>
+                <nav>
+                    <ul class="footer__menu">
+                        <li class="footer__link">Доставка</li>
+                        <li class="footer__link">Оплата</li>
+                        <li class="footer__link">Акции</li>
+                        <li class="footer__link">Политика конфеденциональности</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="footer__item">
+                <div class="footer__title">Оплата</div>
+                <div class="footer__descr">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit sagittis.</div>
+                <div class="footer__icons">
+                    <a href="#"><img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/visa.png' ?>" alt="visa" class="footer__icon"></a>
+                    <a href="#"><img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/mastercard.png' ?>" alt="mastercard" class="footer__icon"></a>
+                </div>
+            </div>
+            <div class="footer__item">
+                <div class="footer__title">Контакты</div>
+                <div class="footer-contacts">
+                    <div class="footer-contacts__item">
+                        <img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/contacts/phone.svg' ?>" alt="phone" class="footer-contacts__icon">
+                        <a href="tel:88122345655" class="footer-contacts__phone">8 (812) 234-56-55</a>
+                    </div>
+                        <div class="footer-contacts__item">
+                            <img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/contacts/phone.svg' ?>" alt="phone" class="footer-contacts__icon">
+                            <a href="tel:88122345655" class="footer-contacts__phone">8 (812) 234-56-55</a>
+                        </div>
+                    <div class="footer-contacts__item">
+                        <img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/contacts/mail.svg' ?>" alt="mail" class="footer-contacts__icon">
+                        <a href="mailto:ojjo@ojjo.ru" class="footer-contacts__mail">ojjo@ojjo.ru</a>
+                    </div>   
+                </div>
+            </div>
+            <div class="footer__item">
+                <div class="footer__title">Социальные сети</div>
+                <div class="footer__descr">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit sagittis.</div>
+                <div class="footer__icons">
+                    <a href="#"><img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/social/youtube.png' ?>" alt="visa" class="footer__icon"></a>
+                    <a href="#"><img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/social/vk.png' ?>" alt="vk" class="footer__icon"></a>
+                    <a href="#"><img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/social/facebook.png' ?>" alt="facebook" class="footer__icon"></a>
+                    <a href="#"><img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/social/telegram.png' ?>" alt="telegram" class="footer__icon"></a>
+                    <a href="#"><img src="<?php echo bloginfo('template_url') . '/assets/img/icons/footer/social/etsy.png' ?>" alt="etsy" class="footer__icon"></a>  
+                </div>
+            </div>
+        </div>
+        <div class="footer__divider"></div>
+        <div class="footer-bottom">
+            <div class="footer-bottom__item">(c) 2021 OJJO jewerly</div>
+            <div class="footer-bottom__item">Договор публичной офферты</div>
+            <div class="footer-bottom__item">Контрагентам</div>
+            <div class="footer-bottom__item">Made by Amina Batchaeva</div>
+        </div>
+    </div>
+</footer>
 <?php wp_footer();?>
 </body>
 </html>
